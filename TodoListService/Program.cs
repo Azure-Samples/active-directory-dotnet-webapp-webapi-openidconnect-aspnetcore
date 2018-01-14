bdd265dbf326ca3c95151392184afc33e3cc225e
Jean-Marc Prieur <jmprieur@microsoft.com>
2018-01-14 13:32:17 -0800

    Initial commit for ASP.NET core 2.0 Taking: -  the TodoListWebApp from active-directory-dotnet-webapp-openidconnect-aspnetcore - the TodoListService from active-directory-dotnet-native-aspnetcore

--- TodoListService/Program.cs
+++ TodoListService/Program.cs
@@ -3,24 +3,23 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace TodoListService
 {
     public class Program
     {
-        // Entry point for the application.
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
-
-            host.Run();
-        }
     }
 }
